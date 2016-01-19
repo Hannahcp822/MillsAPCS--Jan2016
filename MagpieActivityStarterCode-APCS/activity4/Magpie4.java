00255e54102ce64b64e66c05375afa8754b9b1f8
Hannahcp822 <hannahpang@Hannahs-Laptop.local>
2016-01-18 18:35:41 -0800

    Magpie Activity 4

--- MagpieActivityStarterCode-APCS/activity4/Magpie4.java
+++ MagpieActivityStarterCode-APCS/activity4/Magpie4.java
@@ -10,6 +10,9 @@ package activity4;
  *</li></ul>
  * @author Laurie White
  * @version April 2012
+ * 
+ * Modified by Hannah Pang
+ * January 18, 2016
  *
  */
 public class Magpie4
@@ -33,6 +36,7 @@ public class Magpie4
 	public String getResponse(String statement)
 	{
 		String response = "";
+		
 		if (statement.length() == 0)
 		{
 			response = "Say something, please.";
@@ -55,8 +59,29 @@ public class Magpie4
 		{
 			response = transformIWantToStatement(statement);
 		}
+		
+		else if (findKeyword(statement, "I want", 0) >= 0)
+		{
+		    response = transformIWantStatement(statement);
+		}
 
-		else
+		else if (findKeyword(statement, "I", 0) >= 0)
+		{
+		    // Look for a two word (I <something> you)
+		    // pattern
+		    int psnI = findKeyword(statement, "I", 0);
+		    
+		    if (findKeyword(statement, "you", psnI) >= 0)
+		    {
+		        response = transformIYouStatement(statement);
+		    }
+		    else
+		    {
+		        response = getRandomResponse();
+		    }
+		}
+		
+		else 
 		{
 			// Look for a two word (you <something> me)
 			// pattern
@@ -72,6 +97,7 @@ public class Magpie4
 				response = getRandomResponse();
 			}
 		}
+		
 		return response;
 	}
 	
@@ -92,13 +118,35 @@ public class Magpie4
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
+		
 		int psn = findKeyword (statement, "I want to", 0);
+		
 		String restOfStatement = statement.substring(psn + 9).trim();
 		return "What would it mean to " + restOfStatement + "?";
 	}
 
-	
-	
+	/**
+     * Take a statement with "I want <something>." and transform it into 
+     * "Would you be really happy if you had <something>?"
+     * @param statement the user statement, assumed to contain "I want"
+     * @return the transformed statement
+     */
+    private String transformIWantStatement(String statement)
+    {
+        //  Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals("."))
+        {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+        
+        int psn = findKeyword(statement, "I want", 0);
+        
+        String restOfStatement = statement.substring(psn + 6).trim();
+        return "Would you be really happy if you had " + restOfStatement + "?";
+    }
+
 	/**
 	 * Take a statement with "you <something> me" and transform it into 
 	 * "What makes you think that I <something> you?"
@@ -124,10 +172,29 @@ public class Magpie4
 		return "What makes you think that I " + restOfStatement + " you?";
 	}
 	
-	
-
-	
-	
+	/** 
+     * Take a statement with "I <something> you" and transform it into
+     * "Why do you <something> me?"
+     * @param statement the user statement, assumed to contain "I" followed by "you"
+     * @return the transformed statement
+     */
+    private String transformIYouStatement(String statement)
+    {
+        //  Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals("."))
+        {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+   
+        int psnOfI = findKeyword (statement, "I", 0);
+        int psnOfYou = findKeyword (statement, "you", psnOfI + 1);
+        
+        String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+        return "Why do you " + restOfStatement + " me?";
+    }
+    
 	/**
 	 * Search for one word in phrase. The search is not case
 	 * sensitive. This method will check that the given goal
@@ -205,8 +272,6 @@ public class Magpie4
 		return findKeyword (statement, goal, 0);
 	}
 	
-
-
 	/**
 	 * Pick a default response to use if nothing else fits.
 	 * @return a non-committal string
